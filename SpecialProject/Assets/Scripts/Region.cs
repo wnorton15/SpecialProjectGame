41e840315b81df90b6ac9410ad41bc5b3731c21e
Will Norton <norton402@morrisville.edu>
2020-09-10 22:54:58 -0400

    started on region.cs script

---
+++ SpecialProject/Assets/Scripts/Region.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Region : MonoBehaviour
+{
+    [SerializeField] int trees;
+    [SerializeField] int infectedTrees = 0;
+    [SerializeField] float percentInfected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

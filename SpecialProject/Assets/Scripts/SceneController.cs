51b6b5c6273622b61c786dd1ec7a09287a79184a
Will Norton <norton402@morrisville.edu>
2020-09-17 21:54:02 -0400

    Added functionality to the start and end screen

---
+++ SpecialProject/Assets/Scripts/SceneController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        // creates index to find active scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        //load the next scene
+        if (currentSceneIndex < 2)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            //back to start
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+

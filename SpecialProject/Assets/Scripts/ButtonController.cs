e62b8fdef4c707cfabeaa91cf03f227eb90e90c0
Will Norton <norton402@morrisville.edu>
2020-09-25 11:10:09 -0400

    All regions are clickable buttons

---
+++ SpecialProject/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    [SerializeField] Button[] allRegionButtons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Button button in allRegionButtons)
+        {
+            button.image.alphaHitTestMinimumThreshold = .1f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void abc()
+    {
+        Debug.Log("abc");
+    }
+}
--- SpecialProject/Assets/Scripts/Region.cs
+++ SpecialProject/Assets/Scripts/Region.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Region : MonoBehaviour
 {
     [SerializeField] int trees;
     [SerializeField] int infectedTrees = 0;
     [SerializeField] float percentInfected;
+    [SerializeField] Text infectionCounter;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,7 @@ public class Region : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //set infection counter 
+        infectionCounter.text = infectedTrees.ToString();
     }
 }

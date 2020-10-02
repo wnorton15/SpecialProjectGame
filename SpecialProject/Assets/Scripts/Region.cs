f1330afcb183f7d2a026fa79d818f037a88e5d14
Will Norton <norton402@morrisville.edu>
2020-10-01 20:38:56 -0400

    infection spreads within the regions

--- SpecialProject/Assets/Scripts/Region.cs
+++ SpecialProject/Assets/Scripts/Region.cs
@@ -9,6 +9,8 @@ public class Region : MonoBehaviour
     [SerializeField] int infectedTrees = 0;
     [SerializeField] float percentInfected;
     [SerializeField] Text infectionCounter;
+    float timeSinceSpread = Mathf.Infinity;
+    float timeBetweenSpread = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,26 @@ public class Region : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (timeSinceSpread > timeBetweenSpread)
+        {
+            if (infectedTrees > 0 && infectedTrees < 10)
+            {
+                infectedTrees += 1;
+            }
+            else if (infectedTrees > 0 && infectedTrees < 100000)
+            {
+                infectedTrees += Mathf.RoundToInt(infectedTrees * .1f);
+            }
+            else if (infectedTrees > 0)
+            {
+                infectedTrees += Mathf.RoundToInt(infectedTrees * .025f);
+            }
+            timeSinceSpread = 0;
+        }
+        else
+        {
+            timeSinceSpread += Time.deltaTime;
+        }
     }
 
     public void ShowInfo()

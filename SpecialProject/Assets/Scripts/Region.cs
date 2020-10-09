5c959d572597050d88b8e3521724c1c8b2157c8d
Will Norton <norton402@morrisville.edu>
2020-10-08 21:48:27 -0400

    updated product backlog for current sprint and prepared to start on the sprint

--- SpecialProject/Assets/Scripts/Region.cs
+++ SpecialProject/Assets/Scripts/Region.cs
@@ -11,6 +11,7 @@ public class Region : MonoBehaviour
     [SerializeField] Text infectionCounter;
     float timeSinceSpread = Mathf.Infinity;
     float timeBetweenSpread = 5f;
+    bool showing = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,30 +24,49 @@ public class Region : MonoBehaviour
     {
         if (timeSinceSpread > timeBetweenSpread)
         {
-            if (infectedTrees > 0 && infectedTrees < 10)
-            {
-                infectedTrees += 1;
-            }
-            else if (infectedTrees > 0 && infectedTrees < 100000)
-            {
-                infectedTrees += Mathf.RoundToInt(infectedTrees * .1f);
-            }
-            else if (infectedTrees > 0)
-            {
-                infectedTrees += Mathf.RoundToInt(infectedTrees * .025f);
-            }
-            timeSinceSpread = 0;
+            IncreaseInfection();
         }
         else
         {
             timeSinceSpread += Time.deltaTime;
         }
+
+        if (showing)
+        {
+            infectionCounter.text = "Healthy Trees: " + (trees - infectedTrees).ToString() +
+                "\nInfected Trees: " + infectedTrees.ToString() +
+                "\nPercent Infected: " + ((infectedTrees / trees) * 100).ToString() + "%";
+        }
+    }
+
+    private void IncreaseInfection()
+    {
+        if (infectedTrees > 0 && infectedTrees < 10)
+        {
+            infectedTrees += 1;
+        }
+        else if (infectedTrees > 0 && infectedTrees < 100000)
+        {
+            infectedTrees += Mathf.RoundToInt(infectedTrees * .1f);
+        }
+        else if (infectedTrees > 0)
+        {
+            infectedTrees += Mathf.RoundToInt(infectedTrees * .025f);
+        }
+        timeSinceSpread = 0;
     }
 
     public void ShowInfo()
     {
-        infectionCounter.text = "Healthy Trees: " + (trees - infectedTrees).ToString() +
-            "\nInfected Trees: " + infectedTrees.ToString() +
-            "\nPercent Infected: " + ((infectedTrees/trees)*100).ToString() + "%";
+        foreach (Region region in FindObjectsOfType<Region>())
+        {
+            region.UnshowInfo();
+        }
+        showing = true;
+    }
+
+    public void UnshowInfo()
+    {
+        showing = false;
     }
 }

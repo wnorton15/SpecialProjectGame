29f22d9c6aafa2c59db76d37b14f61e0304bd255
Will Norton <norton402@morrisville.edu>
2020-10-15 21:09:01 -0400

    created difficulty settings

--- SpecialProject/Assets/Scripts/Region.cs
+++ SpecialProject/Assets/Scripts/Region.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,8 +10,11 @@ public class Region : MonoBehaviour
     [SerializeField] int infectedTrees = 0;
     [SerializeField] float percentInfected;
     [SerializeField] Text infectionCounter;
+    [SerializeField] Region[] surroundingRegions;
+    float timeBetweenInfectingNewRegion = 10f;
+    float timeSinceInfectedNewRegion = 0f;
     float timeSinceSpread = Mathf.Infinity;
-    float timeBetweenSpread = 5f;
+    float timeBetweenSpread = 2f;
     bool showing = false;
 
     // Start is called before the first frame update
@@ -22,6 +26,7 @@ public class Region : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //increase infection 
         if (timeSinceSpread > timeBetweenSpread)
         {
             IncreaseInfection();
@@ -31,6 +36,17 @@ public class Region : MonoBehaviour
             timeSinceSpread += Time.deltaTime;
         }
 
+        //timer for spreading to new region 
+        if (timeSinceInfectedNewRegion > timeBetweenInfectingNewRegion)
+        {
+            SpreadToNewRegion();
+        }
+        else if (infectedTrees > (trees / 2))
+        {
+            timeSinceInfectedNewRegion += Time.deltaTime;
+        }
+
+        //update counter
         if (showing)
         {
             infectionCounter.text = "Healthy Trees: " + (trees - infectedTrees).ToString() +
@@ -39,19 +55,57 @@ public class Region : MonoBehaviour
         }
     }
 
+    private void SpreadToNewRegion()
+    {
+        //spread to first uninfected region adjacent to the region
+        foreach (Region region in surroundingRegions)
+        {
+            if (region.GetInfectedTrees() > 0)
+            {
+
+            } else
+            {
+                region.Infect();
+                return;
+            }
+        }
+        return;
+    }
+
     private void IncreaseInfection()
     {
+        float difficultyMultiplier = 1;
+        int difficulty = FindObjectOfType<Settings>().getDifficulty();
+        if (difficulty == 1)
+        {
+            difficultyMultiplier = 1;
+        }
+        else if (difficulty == 2)
+        {
+            difficultyMultiplier = 1.3;
+        }
+        else if (difficulty == 3)
+        {
+            difficultyMultiplier = 1.6;
+        }
+        //increase the infection faster as the infection grows
         if (infectedTrees > 0 && infectedTrees < 10)
         {
             infectedTrees += 1;
         }
         else if (infectedTrees > 0 && infectedTrees < 100000)
         {
-            infectedTrees += Mathf.RoundToInt(infectedTrees * .1f);
+            infectedTrees += Mathf.RoundToInt(infectedTrees * .1f * difficultyMultiplier);
         }
         else if (infectedTrees > 0)
         {
-            infectedTrees += Mathf.RoundToInt(infectedTrees * .025f);
+            infectedTrees += Mathf.RoundToInt(infectedTrees * .025f * difficultyMultiplier);
+        }
+
+        //cap the infection
+        if (infectedTrees > trees)
+        {
+            infectedTrees = trees;
         }
         timeSinceSpread = 0;
     }
@@ -68,5 +122,15 @@ public class Region : MonoBehaviour
     public void UnshowInfo()
     {
         showing = false;
+    }
+
+    public int GetInfectedTrees()
+    {
+        return infectedTrees;
+    }
+
+    public void Infect()
+    {
+        infectedTrees++;
     }
 }
---
+++ SpecialProject/Assets/Scripts/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    //1 - easy 2 - medium 3 - hard
+    [SerializeField] [Range(1, 3)] int difficulty = 1;
+    
+    public int getDifficulty()
+    {
+        return difficulty;
+    }
+}
